c169db668c404a6ad405df964c90a7e0c3c0359e
liujunxiang0076 <liujunxiang0076@gmail.com>
2024-11-18 08:39:14 +0800

    更新

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -195,16 +195,15 @@ export default withPwa(
       },
     },
     build: {
-      chunkSizeWarningLimit: 1600,
       rollupOptions: {
         output: {
-          manualChunks(id) {     
+          manualChunks(id) {
             if (id.includes('node_modules')) {
-              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+              return 'vendor';
             }
           }
         }
       }
-    },
+    }
   }),
 );
